b237b93b7e9e2b96e6edd978a5713cbdf75632bc
Janae Quinones <janaemassa@gmail.com>
2021-02-24 23:43:40 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(count) {
+    // while the argument is greater than 0
+    while (count > 0) { 
+        // write the argument
+        console.log(count);
+        // subtract the argument by 1
+        count -= 1
+    }
+    // invoke console.log, or write the console.log()
+    console.log(count)
+    // repeat until condition is no longer satisfied
+}
